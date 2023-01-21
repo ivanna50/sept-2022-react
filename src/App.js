117122d96d93058ecd5b86a7f4e3ad9dfab6fed5
ivanna 50 <jana.bagin@gmail.com>
2023-01-21 19:21:42 +0200

    add App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 
 import React from 'react';
-// import {Simpson} from "./components/Simpson/Simpson";
+import {Simpson} from "./components/Simpson/Simpson";
 import {Characters} from "./components/characters/characters";
 
 const App = () => {
     return (
         <div>
 
-            {/*<Simpson name={'Merge'} surname={'Simpson'} image={'https://toleranten.files.wordpress.com/2011/03/original.jpg'}/>*/}
-            {/*<Simpson name={'Bart'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png'}/>*/}
-            {/*<Simpson name={'Megi'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/ru/thumb/9/9d/Maggie_Simpson.png/200px-Maggie_Simpson.png'}/>*/}
-            <Characters/>
+            <Simpson name={'Merge'} surname={'Simpson'} image={'https://toleranten.files.wordpress.com/2011/03/original.jpg'}/>
+            <Simpson name={'Bart'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png'}/>
+            <Simpson name={'Megi'} surname={'Simpson'} image={'https://upload.wikimedia.org/wikipedia/ru/thumb/9/9d/Maggie_Simpson.png/200px-Maggie_Simpson.png'}/>
+            {/*<Characters/>*/}
         </div>
     );
 };
